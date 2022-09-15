15fb4e10fb63cb24cd2847dfa848be0e70e87c72
= <ethan.shealey@gmail.com>
2022-09-15 09:59:18 -0400

    added cors

--- classA/app.js
+++ classA/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 
 var app = express();
+var cors = require(cors)
+
+app.use(cors)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
